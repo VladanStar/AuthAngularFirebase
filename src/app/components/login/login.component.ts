de9561674cc3a0168541c5b4ab46565e524b104f
VladanStar <vladancupric@gmail.com>
2023-03-23 15:22:20 +0100

    Add

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,14 @@ export class LoginComponent implements OnInit {
   username: any;
   password: any;
 
-  constructor() {}
+  constructor( private authService:AuthService) {}
 
   ngOnInit(): void {}
 login(){
-
+this.authService.login(this.username, this.password).then(()=>{
+alert('login is successfully')
+}).catch(err=>{
+alert(err);
+})
 }
 }
