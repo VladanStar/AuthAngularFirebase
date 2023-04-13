90c3ca3bfb0f1931cac35faf5d6e85b364b88c1a
VladanStar <vladancupric@gmail.com>
2023-04-13 19:28:47 +0200

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
 
-  {path:"", redirectTo:"login", pathMatch:'full'},
+   {path:"", redirectTo:"login", pathMatch:'full'},
   {path:"login", component:LoginComponent},
 {path:"dashboard", component:DashboardComponent},
 {path:"register", component:RegisterComponent}
